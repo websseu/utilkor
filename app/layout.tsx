60c357bb0ec03e9d255c537dc0e4f55b41f58d03
웹스토리보이 <webstoryboy@naver.com>
2024-11-04 08:59:27 +0900

    2024.11.04

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -29,6 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <Analytics />
       <body>{children}</body>
     </html>
   );
